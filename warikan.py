e9a803fb1ae9deceaf16ddd1962bbf3381b1f5ff
noopy0415 <noopy0415@gmail.com>
2018-11-18 10:12:31 +0900

    リファクタリング
    割り勘を関数化

--- warikan.py
+++ warikan.py
@@ -1,23 +1,29 @@
-# Table1:1500円で3人
-amount = 1500
-number_of_people = 3
-print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
-
-# Table2:2000円で3人
-amount = 2000
-number_of_people = 3
-print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
-
-# Table3:3647円で4人
-amount = 3647
-number_of_people = 4
-print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+# # Table1:1500円で3人
+# amount = 1500
+# number_of_people = 3
+# print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+#
+# # Table2:2000円で3人
+# amount = 2000
+# number_of_people = 3
+# print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
+#
+# # Table3:3647円で4人
+# amount = 3647
+# number_of_people = 4
+# print(f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円")
 
 
 def warikan(price, count):
-    return amount // number_of_people, amount % number_of_people
+    return price // count, price % count
 
 
-result
+while 1:
+    price = input("割り勘する金額を入力してください : ")
+    if price == "": break
 
-print(f"1人あたり: {warikan(1500,3)}")
+    count = input("割り勘する人数を入力してください : ")
+    if count == "": break
+
+    result = warikan(int(price), int(count))
+    print(f"1人あたり: {str(result[0])}円, 端数: {str(result[1])}円")
---
+++ warikan2.py
@@ -0,0 +1,11 @@
+# # Table1:1500円で3人 -> 500, 0
+# # Table2:2000円で3人 -> 666, 1
+# # Table3:3647円で4人
+
+def warikan(amount, number_of_people):
+    return f"1人あたり: {amount // number_of_people}円, 端数: {amount % number_of_people}円"
+
+
+print(warikan(amount=1500, number_of_people=3))
+print(warikan(amount=2000, number_of_people=3))
+print(warikan(amount=3000, number_of_people=4))
